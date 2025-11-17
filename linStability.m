33faf432d062a3bccecc16ab57a8caa36bc2ff57
LennartMende <166758749+LennartMende@users.noreply.github.com>
2025-11-17 21:12:56 +0100

    Delete linStability.m

--- linStability.m
+++
@@ -1,8 +0,0 @@
-%jac: allg. Jacobi-Matrix, opJac: Jac-Matrix am AP,
-%eigenvalues: Eigenwerte von opJac, stabFlag: 0 = instabil, 1 = stabil
-function [jac, opJac, eigenvalues] = linStability(x, f, eqPos)
-    disp(f)
-    jac = jacobian(f);
-    opJac = subs(jac, [x(1), x(2), x(3)], [eqPos.x1, eqPos.x2, eqPos.x3]);
-    eigenvalues = eig(opJac);
-end
