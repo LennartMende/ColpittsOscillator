839eaaf81de115bdddff75a78d8d6302461e004f
LennartMende <166758749+LennartMende@users.noreply.github.com>
2026-01-03 11:55:54 +0100

    Add files via upload

--- EALinearisierung.m
+++ EALinearisierung.m
@@ -1,25 +1,38 @@
+R = 1e+3;
+L = 100e-3;
+C1 = 100e-9;
+C2 = 1e-6;
+
+k  = 1.380649e-23;
+e  = 1.602176634e-19;
+T  = 25 + 273.15;
+UT = k*T/e;
+I_S = 10e-9;
+
+syms U1 U2 IL U0 I0
+x = [U1, U2, IL];
 %Zerlegung in System-, Ausgangs- und Eingangswirkung
 f_ea = [
-    -I_S*exp(-x2/U_T)/C1 + x3/C1;
-    x3/C2;
-    -x1/L - x2/L - R*x3/L
+    -I_S*exp(-U2/UT)/C1 + IL/C1;
+    IL/C2;
+    -U1/L - U2/L - R*IL/L
 ]
 g_ea = [
     0, 0;
-    0, -I0/C2;
+    0, -1/C2;
     1/L, 0;
 ]
-g_ea_u1 = [
+g_ea_U0 = [
     0;
     0;
     1/L;
 ]
-g_ea_u2 = [
+g_ea_I0 = [
     0;
     -I0/C2;
     0;
 ]
-h_ea = x1 %for y = xi set h_ea at position i to 1
+h_ea = IL %for y = xi set h_ea at position i to 1
 
 %Lie-Ableitungen berechnen
 Lf = h_ea;
@@ -28,7 +41,7 @@ r = 0;
 L_fh = Lf;
 
 while true
-    LgLf = jacobian(Lf, x) * g_ea_u1;
+    LgLf = jacobian(Lf, x) * g_ea_I0;
     if LgLf ~= 0
         break;
     end
@@ -41,6 +54,6 @@ r = r + 1
 
 
 syms v
-L_nom = v - L_fh;  % letzte Lf
+L_nom = v - Lf_h;  % letzte Lf
 L_denom = LgLf;
 L = L_nom / L_denom;
