78aab8a54884d43935862a0ff3f3491adc343886
LennartMende <166758749+LennartMende@users.noreply.github.com>
2026-01-22 18:21:27 +0100

    Add files via upload

--- EALinearisierungMISO.m
+++ EALinearisierungMISO.m
@@ -29,7 +29,7 @@ g_ea_U0 = [
 ]
 g_ea_I0 = [
     0;
-    -I0/C2;
+    -1/C2;
     0;
 ]
 h_ea = U2 %for y = xi set h_ea at position i to 1
@@ -39,13 +39,13 @@ Lf = h_ea;
 Lf_h = Lf;
 r = 0;
 
-Lf_r = sym(zeros(10,1))
-Lf_r(1) = Lf_h
+Lf_r_init = sym(zeros(10,1))
+Lf_r_init(1) = Lf_h
 
 
 while true
     LgLf = jacobian(Lf, x) * g_ea % x = [U1; U2; IL]
-    if LgLf ~= 0
+    if (LgLf(1) ~= 0 || LgLf(2) ~= 0)
         % vermutlich notwendig, um L_f^r h anstelle von L_f^{(r-1)} h zu
         % erhalten
         Lf = jacobian(Lf, x) * f_ea
@@ -55,11 +55,28 @@ while true
     end
     Lf = jacobian(Lf, x) * f_ea % x = [U1; U2; IL], Lf = IL/C2
     Lf_h = Lf
-    Lf_r(r+2) = Lf_h
+    Lf_r_init(r+2) = Lf_h
     r = r + 1
 end
 
 r = r + 1
+
+if r == 1
+    Lf_r = zeros(1,1);
+    Lf_r = Lf_r_init(1);
+
+    % Pole für Regelung:
+    p = [-3];
+end
+
+if r == 2
+    Lf_r = zeros(2,1);
+    Lf_r = Lf_r_init(1);
+    Lf_r = Lf_r_init(2);
+
+    % Pole für Regelung:
+    p = [-2-2*i, -2+2*i];
+end
 
 phi = Lf_r
 disp(Lf_r)
@@ -99,8 +116,6 @@ C(1) = 1
 
 %syms U2_ref Kp
 %v = Kp * (U2_ref - U2);
-
-p = [-3, -2+1j, -2-1j];
 
 K = place(A, B, p)
 
